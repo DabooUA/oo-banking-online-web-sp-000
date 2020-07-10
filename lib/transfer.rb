b5c8bff77fa1805f54613b5cae4852f13783682f
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-10 18:39:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   def execute_transaction
 
     if @sender.balance > @amount && @status == "pending"
-       @valid?
+       @valid
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
