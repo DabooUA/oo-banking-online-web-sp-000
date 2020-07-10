cb912d6ae2a4f3e36a9d8e7a9ac53667a6219caf
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-10 18:49:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,6 @@ class Transfer
 
     if
        @sender.balance > @amount && @status == "pending" && valid?
-
        @sender.balance -= @amount
        @receiver.balance += @amount
        @status = "complete"
